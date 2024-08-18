504a03ba8e1c0d58563b0fc9558b07c6ceafb298
ataher83 <taher83@gmail.com>
2024-08-18 21:03:12 +0600

    index.js updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
         'http://localhost:5173', 
         'http://localhost:5174', 
         'http://localhost:5175', 
-        'https://the-natural-agro.web.app'
+        'https://natural-agro.web.app'
       ],
       credentials: true,
   }),
