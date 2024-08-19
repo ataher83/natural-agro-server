71051dc028a56dbc2e78e82b7357f10785044153
ataher83 <taher83@gmail.com>
2024-08-19 23:33:50 +0600

    route to get products

--- index.js
+++ index.js
@@ -38,105 +38,20 @@ async function run() {
     // await client.connect();  
 
 
-  const queriesCollection = client.db('altProductsDB').collection('queries');
-  const recommendationsCollection = client.db('altProductsDB').collection('recommendations');
+  const productsCollection = client.db('natural-agro').collection('products');
 
 
 
 
-// queries related api
+// products related api
 
-  app.get('/queries', async(req, res) => {
-      const cursor = queriesCollection.find() 
+  app.get('/products', async(req, res) => {
+      const cursor = productsCollection.find() 
       const result = await cursor.toArray() 
       res.send(result) 
   })
 
 
-  app.get('/queries/:id', async(req, res) => {
-      const id = req.params.id 
-      const query = { _id: new ObjectId(id)} 
-      const result = await queriesCollection.findOne(query)
-      res.send(result) 
-  })
-
-
-  app.post('/queries', async(req, res) => {
-      const newQuery = req.body
-      // console.log(newQuery)
-      const result = await queriesCollection.insertOne(newQuery)
-      res.send(result) 
-  })
-
-
-  app.put('/queries/:id', async(req, res) => {
-      const id = req.params.id 
-      const filter = { _id: new ObjectId(id)} 
-      const options = { upsert: true }
-      const updatedQuery = req.body
-      // The query variable is unnecessary. You can directly use $set in the update operation /cgp
-      const query = {
-          $set: {
-            productImage: updatedQuery.productImage,
-            productName: updatedQuery.productName,
-            productBrand: updatedQuery.productBrand,
-            queryTitle: updatedQuery.queryTitle,
-            boycottingReasonDetails: updatedQuery.boycottingReasonDetails,
-            userEmail: updatedQuery.userEmail,
-            userName: updatedQuery.userName,
-            userImage: updatedQuery.userImage,
-            currentDateAndTime: updatedQuery.currentDateAndTime,
-            recommendationCount: updatedQuery.recommendationCount
-          }
-      }
-
-      const result = await queriesCollection.updateOne(filter, query, options)
-      res.send(result) 
-  })
-
-  app.delete('/queries/:id', async(req, res) => {
-      const id = req.params.id 
-      const query = { _id: new ObjectId(id)} 
-      const result = await queriesCollection.deleteOne(query)
-      res.send(result) 
-  })
-
-
-
-
-
-// // recommendations related api
-
-  app.get('/recommendations', async(req, res) => {
-      const cursor = recommendationsCollection.find() 
-      const result = await cursor.toArray() 
-      res.send(result) 
-  })
-
-
-  app.get('/recommendations/:id', async(req, res) => {
-      const id = req.params.id 
-      const query = { _id: new ObjectId(id)} 
-      const result = await recommendationsCollection.findOne(query)
-      res.send(result) 
-  })
-
-  app.post('/recommendations', async(req, res) => {
-      const newRecommendation = req.body
-      const result = await recommendationsCollection.insertOne(newRecommendation)
-      res.send(result) 
-  })
-
-
-
-
-
-  app.delete('/recommendations/:id', async(req, res) => {
-      const id = req.params.id 
-      const query = { _id: new ObjectId(id)} 
-      const result = await recommendationsCollection.deleteOne(query)
-      res.send(result) 
-  })
 
 
 
